cebb410bb03f16703bb66ac56d05533d76f2eef8
Dennis Snijder <dennissnijder97@gmail.com>
2017-09-30 17:16:47 +0200

    Added fix for JSON posting

--- app/app.php
+++ app/app.php
@@ -5,17 +5,24 @@ use BunqWeb\Provider\Controller\DashboardControllerProvider;
 use BunqWeb\Provider\Service\BunqServiceProvider;
 use BunqWeb\Provider\Service\ConfigServiceProvider;
 use BunqWeb\Provider\Service\RepositoryServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__ . '/bootstrap.php';
 
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
 $app->register(new Silex\Provider\TwigServiceProvider, [
     'twig.path' => RESOURCES_PATH . '/views',
 ]);
 $app->register(new \Silex\Provider\SessionServiceProvider);
-
 
 //application
 $app->register(new ConfigServiceProvider);
